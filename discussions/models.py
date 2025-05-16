deed114ebc56cc8c0f6b3ddd79202d1ce11768e4
Jb-rown <brownjohn9870@gmail.com>
2025-05-16 08:02:04 +0300

    discussions/models.py

--- discussions/models.py
+++ discussions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from courses.models import Course
+from accounts.models import User
 
-# Create your models here.
+class Thread(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Post by {self.author.username} in {self.thread.title}"
